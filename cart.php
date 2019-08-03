d7ee44aa2bc01c1b5f1da56b769f003effd112ec
chhavisinghal <priyalsinghal2@gmail.com>
2019-08-04 01:37:36 +0530

    Traveeling site

---
+++ cart.php
@@ -0,0 +1,28 @@
+<?php
+include 'session.php';
+if(loggedin())
+{
+include 'connect.php';
+$user=$_SESSION['username1'];
+$name=$_POST["name"];
+$url=$_POST["img"];
+$price=$_POST["price"];
+$city=$_POST["city"];
+$name=str_replace('.', " ", $name);
+$sql="INSERT INTO `my_cart`(`user`,`name`,`price`,`img`,`city`)VALUES('$user','$name','$price','$url','$city')";
+$var = mysqli_query($conn,$sql);
+if($var>0)
+{
+	header('LOCATION:first.php');
+}
+else
+{
+	echo $sql;
+}
+}
+else
+{
+	 header('LOCATION:index.php');
+}
+
+?>
---
+++ cart_hotel_display.php
@@ -0,0 +1,33 @@
+<?php
+
+include 'session.php';
+if(loggedin())
+{
+include 'connect.php';
+include 'hfunction.php';
+
+$user=$_SESSION['username1'];
+$q="SELECT * FROM `my_cart` WHERE user='$user'";
+$query = mysqli_query($conn,$q);
+	//echo $q;
+while($row=mysqli_fetch_array($query))
+    {
+
+    $name=$row['name'];
+   
+    $price=$row['price'];
+    $url=$row['img'];
+    $city=$row['city'];
+    hotels($name,$url,$price,$city);
+
+}
+
+    }
+
+else
+{
+	 header('LOCATION:index.php');
+}
+
+
+?>
